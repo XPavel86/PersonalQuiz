b52de31959a283f9d43560d15d8246f14925b227
Pavel <rezusx@mail.ru>
2024-03-04 02:50:27 +0530

    fix

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -25,19 +25,21 @@ final class ResultViewController: UIViewController {
             for answer in answerArray {
                 animalCounts[answer.animal, default: 0] += 1
             }
-
+        
             let maxCount = animalCounts.values.max()
             let frequentAnswers = animalCounts.filter { $0.value == maxCount }
 
             if frequentAnswers.count == 1 {
                 if let (animal, _) = frequentAnswers.first {
-                    resultLabel.text! += String("Вы - \(animal.rawValue)!")
+                    resultLabel.text! = String("Вы - \(animal.rawValue)!")
                     definitionLabel.text = animal.definition
                 }
             } else {
-                for (animal, _) in frequentAnswers {
-                    resultLabel.text! += String("Вы - \(animal.rawValue)!\n")
-                    definitionLabel.text! += String("\n\n\(animal.definition)")
+                // Сортировка и вывод первого элемента
+                if let (animal, _) = frequentAnswers.sorted(by: {
+                    $0.key.rawValue < $1.key.rawValue }).first {
+                    resultLabel.text! = String("Вы - \(animal.rawValue)!")
+                    definitionLabel.text = animal.definition
                 }
             }
         }
