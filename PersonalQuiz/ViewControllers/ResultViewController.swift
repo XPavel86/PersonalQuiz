8ac802e75e7113dd61ec338c5f4089fb218855ce
Pavel <rezusx@mail.ru>
2024-03-04 23:56:06 +0530

    fix

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -19,24 +19,26 @@ final class ResultViewController: UIViewController {
         
         navigationItem.hidesBackButton = true
         
-        if let answerArray = answers {
-            var animalCounts: [Animal: Int] = [:]
+        if let answers = answers {
+            var animal: Animal? = nil
+            var duplicate = false
             
-            for answer in answerArray {
-                animalCounts[answer.animal, default: 0] += 1
+            for i in 0..<answers.count {
+                for j in (i + 1)..<answers.count {
+                    if answers[i].animal == answers[j].animal {
+                        duplicate = true
+                        animal = answers[i].animal
+                        break
+                    }
+                }
             }
             
-            let maxCount = animalCounts.values.max()
-            let rateAnswers = animalCounts.filter { $0.value == maxCount }
-            
-            let animal = rateAnswers.count == 1
-            ? rateAnswers.first!.key
-            : rateAnswers.sorted(by: {
-                $0.key.rawValue < $1.key.rawValue
-            }).first!.key
-            
-            resultLabel.text = "Вы - \(animal.rawValue)!"
-            definitionLabel.text = animal.definition
+            if !duplicate {
+                animal = answers.sorted(by: {
+                    $0.animal.rawValue < $1.animal.rawValue }).first?.animal
+            }
+            resultLabel.text = "Вы - \(animal!.rawValue)!"
+            definitionLabel.text = animal?.definition
         }
     }
     
@@ -48,3 +50,28 @@ final class ResultViewController: UIViewController {
         print("\(type(of: self)) has been deallocated")
     }
 }
+
+/*
+ 
+Вариант 2
+             if let answerArray = answers {
+                 var animalCounts: [Animal: Int] = [:]
+ 
+                 for answer in answerArray {
+                     animalCounts[answer.animal, default: 0] += 1
+                 }
+ 
+                 let maxCount = animalCounts.values.max()
+                 let rateAnswers = animalCounts.filter { $0.value == maxCount }
+ 
+                 let animal = rateAnswers.count == 1
+                 ? rateAnswers.first!.key
+                 : rateAnswers.sorted(by: {
+                     $0.key.rawValue < $1.key.rawValue
+                 }).first!.key
+ 
+                 resultLabel.text = "Вы - \(animal.rawValue)!"
+                 definitionLabel.text = animal.definition
+             }
+ */
+
